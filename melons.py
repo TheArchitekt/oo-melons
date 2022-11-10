fe6db488bf1b923af52d06977f379f9e5a1878c2
TheArchitekt <richardpennymon@gmail.com>
2022-11-10 16:55:13 -0500

    Add Abstract Melon Order Class Add Government Melon Order Class
    Co-authored-by: jadajones09 <jadajones09@users.noreply.github.com>

--- melons.py
+++ melons.py
@@ -57,3 +57,66 @@ class InternationalMelonOrder:
         """Return the country code."""
 
         return self.country_code
+
+
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from."""
+
+    order_type = None
+
+    tax = 0
+
+    def __init__(self, species, qty):
+        self.species = species
+        self.qty = qty
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        if self.species == "Christmas melon":
+            base_price*= 1.5
+        total = (1 + self.tax) * self.qty * base_price
+        if self.qty < 10 and self.order_type == "international":
+            total += 3
+
+
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    order_type = 'international'
+    tax = 0.17
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty)
+
+        self.country_code = country_code
+
+    def get_country_code(self):
+
+        return self.country_code
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    order_type = 'domestic'
+    tax = 0.08
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    order_type = 'government'
+    tax = 0
+
+    def __init__(self, species, qty, passed_inspection):
+        super().__init__(species, qty)
+
+        self.passed_inspection = False
+
+    def inspection_status(self, passed):
+
+        self.passed_inspection = passed
